e7db6ab5f1dd7b978b819416c3be299de5e96b6e
Edi Sinovčić <edi.sinovcic@digacon-software.com>
2018-02-27 18:19:55 +0100

    Inital commit

---
+++ src/main/java/com/eestec/codeapp/CodeappApplication.java
@@ -0,0 +1,12 @@
+package com.eestec.codeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeappApplication.class, args);
+	}
+}
